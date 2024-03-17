d00f93f5e2366d73ecadb1af404fba39cd06d655
Barkın Balcı <barkin.balci@gmail.com>
2024-03-17 20:20:54 +0300

    feat: new generator ui

--- app/components/Generator.tsx
+++ app/components/Generator.tsx
@@ -1,17 +1,57 @@
-import React, { useState } from "react";
-import { Appbar, Button, Surface, Text, Switch } from "react-native-paper";
+import React, { useState, useEffect } from "react";
+import { Appbar, Button, Surface, Text, Switch, TextInput, MD3DarkTheme, MD3LightTheme, Divider, IconButton } from "react-native-paper";
 import Slider from "@react-native-community/slider";
+import { View, useColorScheme } from "react-native";
+import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
+import Clipboard from "@react-native-community/clipboard";
 
 export default function Generator() {
-  const [passwordLength, setPasswordLength] = useState(8);
-  const [includeSpecials, setIncludeSpecials] = useState(false);
-  const [includeUppercase, setIncludeUppercase] = useState(false);
-  const [includeLowercase, setIncludeLowercase] = useState(false);
-  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [passwordLength, setPasswordLength] = useState(16);
+  const [includeSpecials, setIncludeSpecials] = useState(true);
+  const [includeUppercase, setIncludeUppercase] = useState(true);
+  const [includeLowercase, setIncludeLowercase] = useState(true);
+  const [includeNumbers, setIncludeNumbers] = useState(true);
+  const [password, setPassword] = useState(""); // New state variable for password
 
-  const generatePassword = () => {
-    // Implement your password generation logic here
+  const { theme } = useMaterial3Theme();
+  const colorScheme = useColorScheme();
+  const paperTheme = colorScheme === "dark" ? { ...MD3DarkTheme, colors: theme.dark } : { ...MD3LightTheme, colors: theme.light };
+  
+  const generatePassword = async () => {
+    const specials = "!@#$%^&*";
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+
+    let possibleCharacters = "";
+    if (includeSpecials) possibleCharacters += specials;
+    if (includeLowercase) possibleCharacters += lowercase;
+    if (includeUppercase) possibleCharacters += uppercase;
+    if (includeNumbers) possibleCharacters += numbers;
+
+    if (possibleCharacters.length === 0) {
+      setPassword("");
+      return;
+    }
+
+    let generatedPassword = "";
+    for (let i = 0; i < passwordLength; i++) {
+      const array = new Uint32Array(1);
+      window.crypto.getRandomValues(array);
+      const randomIndex = array[0] % possibleCharacters.length;
+      generatedPassword += possibleCharacters[randomIndex];
+    }
+
+    setPassword(generatedPassword);
   };
+
+  const copyToClipboard = (text) => {
+    Clipboard.setString(text);
+  };
+
+  useEffect(() => {
+    generatePassword();
+  }, [passwordLength, includeSpecials, includeUppercase, includeLowercase, includeNumbers]);
 
   return (
     <Surface elevation={0} style={{ flex: 1 }}>
@@ -19,24 +59,53 @@ export default function Generator() {
         <Appbar.Content title="Generator" />
       </Appbar.Header>
       <Surface style={{ padding: 20, flex: 1 }}>
-        <Text>Password Length: {passwordLength}</Text>
-        <Slider value={passwordLength} onValueChange={setPasswordLength} minimumValue={8} maximumValue={32} step={1} />
+        <View style={{ flexDirection: "row" }}>
+          <View style={{ flex: 1 }}>
+            <Text style={{ opacity: 0.6 }} variant="labelSmall">
+              Password
+            </Text>
+            <Text variant="bodyLarge">{password}</Text>
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <IconButton icon="refresh" onPress={generatePassword} />
+            <IconButton icon="content-copy" onPress={() => copyToClipboard(password)} />
+          </View>
+        </View>
+        <Divider />
+        <View style={{ flexDirection: "row" }}>
+          <Text>Length: {passwordLength}</Text>
+          <Slider
+            thumbTintColor={paperTheme.colors.primary}
+            maximumTrackTintColor={paperTheme.colors.primary}
+            minimumTrackTintColor={paperTheme.colors.primary}
+            value={passwordLength}
+            onValueChange={setPasswordLength}
+            minimumValue={8}
+            maximumValue={128}
+            step={1}
+            style={{ flex: 1 }}
+          />
+        </View>
+        <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text>A-Z</Text>
+            <Switch value={includeUppercase} onValueChange={setIncludeUppercase} />
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text>a-z</Text>
+            <Switch value={includeLowercase} onValueChange={setIncludeLowercase} />
+          </View>
 
-        <Text>Include Special Characters?</Text>
-        <Switch value={includeSpecials} onValueChange={setIncludeSpecials} />
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text>0-9</Text>
+            <Switch value={includeNumbers} onValueChange={setIncludeNumbers} />
+          </View>
 
-        <Text>Include Uppercase Letters?</Text>
-        <Switch value={includeUppercase} onValueChange={setIncludeUppercase} />
-
-        <Text>Include Lowercase Letters?</Text>
-        <Switch value={includeLowercase} onValueChange={setIncludeLowercase} />
-
-        <Text>Include Numbers?</Text>
-        <Switch value={includeNumbers} onValueChange={setIncludeNumbers} />
-
-        <Button mode="contained" onPress={generatePassword} style={{ marginTop: 20 }}>
-          Generate Password
-        </Button>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text>!@#$%^&*</Text>
+            <Switch value={includeSpecials} onValueChange={setIncludeSpecials} />
+          </View>
+        </View>
       </Surface>
     </Surface>
   );
